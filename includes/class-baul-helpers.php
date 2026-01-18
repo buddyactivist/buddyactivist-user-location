a54db35feaa1ee69eaf9a23ee819fb6103c72f71
BuddyActivist <buddyactivist@gmail.com>
2026-01-18 21:15:53 +0100

    Create class-baul-helpers.php

---
+++ includes/class-baul-helpers.php
@@ -0,0 +1,19 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class BAUL_Helpers {
+
+    public static function haversine_distance( $lat1, $lon1, $lat2, $lon2 ) {
+        $earth_radius = 6371;
+        $dLat = deg2rad( $lat2 - $lat1 );
+        $dLon = deg2rad( $lon2 - $lon1 );
+
+        $a = sin($dLat/2) * sin($dLat/2) +
+             cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+             sin($dLon/2) * sin($dLon/2);
+
+        $c = 2 * atan2( sqrt($a), sqrt(1-$a) );
+
+        return $earth_radius * $c;
+    }
+}
